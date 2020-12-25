f3bd0f0b2b6c4332f0745b356a87cd986210f5bb
Yasin Tatar <ytatar9@googlemail.com>
2020-12-25 22:32:23 +0100

    import buefy

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+import Buefy from 'buefy'
+import 'buefy/dist/buefy.css'
+
 Vue.config.productionTip = false;
 
 new Vue({
@@ -10,3 +13,6 @@ new Vue({
   store,
   render: h => h(App)
 }).$mount("#app");
+
+
+Vue.use(Buefy)
