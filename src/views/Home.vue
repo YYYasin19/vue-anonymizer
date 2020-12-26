bca5bae0b003456277ce374f7ca92e52bd1ca622
Yasin Tatar <ytatar9@googlemail.com>
2020-12-26 23:57:51 +0100

    removed steps component for clearer ui view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <b-steps v-model="activeStep">
-      <b-step-item step="1" label="Upload images" icon="folder-multiple-image">
-        <FileUploader></FileUploader>
-      </b-step-item>
-      <b-step-item step="2" label="Select parameters" icon="tune">
-        <SettingsPanel></SettingsPanel>
-      </b-step-item>
-      <b-step-item step="3" label="Review results" icon="image-area-close">
-        Other things too
-      </b-step-item>
-    </b-steps>
+    <FileUploader></FileUploader>
     <!--
 <FileUploader></FileUploader>
     -->
@@ -21,39 +11,17 @@
 <script>
 // @ is an alias to /src
 import FileUploader from "../components/FileUploader.vue";
-import SettingsPanel from "../components/SettingsPanel.vue";
 
 export default {
   name: "Home",
   components: {
-    FileUploader,
-    SettingsPanel
+    FileUploader
   },
   data() {
     return {
       activeStep: 0
     };
   },
-  computed: {
-    steps() {
-      return [
-        {
-          label: "Upload images",
-          content: FileUploader,
-          type: "component"
-        },
-        {
-          label: "Select parameters",
-          content: "Here you can select parameters",
-          type: "html"
-        },
-        {
-          label: "Finish",
-          content: "Here you can send your request",
-          type: "html"
-        }
-      ];
-    }
-  }
+  computed: {}
 };
 </script>
